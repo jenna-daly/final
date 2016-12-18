3d7796ed8b90168b86c92a97e02cedceaffe80c9
Jenna Daly <jenna.daly1@marist.edu>
2016-12-17 23:02:12 -0500

    fixed

--- ex.js
+++ ex.js
@@ -36,15 +36,15 @@ function ex() {
 var N = parseInt(prompt("Enter an odd integer: "));
 // complete the missing program code below:
 var sum = 0
-for(var i=1; i<=N; i+2) {
-sum = sum + 1/i*i;
+for(var i=1; i<=N; i+=2) {
+sum = sum + (1/(i*i));
 }
 
 document.write("1+1/3^2+1/5^2+1/7^2+1/9^2+…+1/ " + N + "^2 is " + sum)
 
 }
 
-ex();
+//ex();
 
 function checkoutcome() {
 // Read the amount of tea and candy
@@ -56,16 +56,51 @@ if(teaamount < 5 || candyamount < 5)
 
 }
 // complete the missing program code below
+else if(teaamount >= candyamount * 2 || candyamount >= teaamount * 2){
+	document.getElementById("show_outcome").innerHTML = "Great";
+}
 else if(teaamount>= 5 && candyamount>=5){
 	document.getElementById("show_outcome").innerHTML = "Good";
 }
 
-else if(teaamount * 2 >= candyamount || candyamount * 2 >= teaamount){
-	document.getElementById("show_outcome").innerHTML = "Great";
-}
+
 
 }
 
 //checkoutcome();
 
+function centerAve(){ 
+var a = [-10, -4, -2, 4, 2, 1];
+// complete the missing program code below:
+var max=a[0]
+var min=a[0]
+var sum=0
 
+for(var i=0; i<=a.length-1; i++){
+	if(a[i]<min) {
+		min=a[i];
+	}
+}
+for(var i=0; i<=a.length-1; i++){
+	if(a[i]>max) {
+		max=a[i];
+	}
+}
+a.splice((a.indexOf(max)),1)
+a.splice((a.indexOf(min)),1)
+
+
+for(var k=0; k<=a.length-1; k++){
+	
+	sum = sum + a[k]
+}
+
+var answer = sum/a.length
+document.write("The average, excluding the largest and smallest values in the array, is " + answer);
+
+
+
+
+}
+
+centerAve();
